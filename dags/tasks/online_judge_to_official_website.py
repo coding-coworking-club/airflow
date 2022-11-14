7319b133d209ba80bf5b240761346558276a44f1
waukin <jimmy83882@gmail.com>
2022-11-14 22:37:16 +0800

    add transform task to apply to ccclub db schema

--- dags/tasks/online_judge_to_official_website.py
+++ dags/tasks/online_judge_to_official_website.py
@@ -8,11 +8,13 @@ import pendulum                 # Declare timezone
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 
-from utils.online_judge_to_offical_website import query_from_online_jundg_db
+from utils.save_csv_from_judge_db import save_csv_from_judge_db
+from utils.transform_to_ccclub_db_schema import transform_to_ccclub_db_schema
+from utils.insert_csv_to_ccclub_db import insert_csv_to_ccclub_db
 
 # Set the timezone, otherwise it might be UTC
 local_tz = pendulum.timezone("Asia/Taipei")
-    
+
 default_args = {
     "owner": "ccclub",
     # "email": ["fake_email@gmail.com", "fake_email_2@gmail.com"],
@@ -28,16 +30,32 @@ default_args = {
 with DAG(
     dag_id="insert_judge_data_into_ccclub_db",
     default_args=default_args,
-    start_date=datetime(2021, 7, 10, 0, 0, 0, 0, tzinfo = local_tz),
+    start_date=datetime(2021, 7, 10, 0, 0, 0, 0, tzinfo=local_tz),
     schedule_interval="0 0 * * *",
-    tags = ["Medium"],
-    catchup = False,
+    tags=["Medium"],
+    catchup=False,
 ) as dag:
     start = DummyOperator(task_id="start")
     end = DummyOperator(task_id="end")
-    query_from_contest = PythonOperator(
-        task_id="contest",
-        python_callable=query_from_online_jundg_db,
+    select = PythonOperator(
+        task_id="save_csv_from_judge_db",
+        python_callable=save_csv_from_judge_db,
+        op_args=["/opt/airflow/dags/sql/select_contest.sql"],
+        provide_context=True,
         dag=dag
     )
-    start >> query_from_contest >> end
+    transform = PythonOperator(
+        task_id="transform_to_ccclub_db_schema",
+        python_callable=transform_to_ccclub_db_schema,
+        op_args=["/opt/airflow/dags/sql/transform_contest.sql"],
+        provide_context=True,
+        dag=dag
+    )
+    insert = PythonOperator(
+        task_id="insert_csv_to_ccclub_db",
+        python_callable=insert_csv_to_ccclub_db,
+        op_args=["/opt/airflow/dags/sql/insert_contest.sql"],
+        provide_context=True,
+        dag=dag
+    )
+    start >> select >> transform >> insert >> end
---
+++ dags/utils/transform_to_ccclub_db_schema.py
@@ -0,0 +1,5 @@
+
+
+def transform_to_ccclub_db_schema(sql, **context):
+    # TODO: transform data to ccclub db schema
+    pass
