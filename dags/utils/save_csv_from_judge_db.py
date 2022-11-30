fedc07543161a10ff6d3db2fbb8f8b0fbb73c5fb
waukin <jimmy83882@gmail.com>
2022-11-30 21:30:10 +0800

    upload raw csv and result csv to s3

--- dags/utils/save_csv_from_judge_db.py
+++ dags/utils/save_csv_from_judge_db.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pandas as pd
 
 from utils.online_judge_to_offical_website import connect_to_judge_db
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 
 def save_csv_from_judge_db(sql, **context):
@@ -19,5 +20,12 @@ def save_csv_from_judge_db(sql, **context):
     col_names = [i[0] for i in pg_cursor.description]
     raw_df = pd.DataFrame.from_records(results, columns=col_names)
     raw_df.to_csv('data/raw.csv', index=False)
+    s3_hook = S3Hook(aws_conn_id="s3_airflow_data")
+    s3_hook.load_file(
+        filename='data/raw.csv',
+        key='raw.csv',
+        bucket_name="ccclub-airflow-data",
+        replace=True
+    )
     raw_csv_path = os.path.abspath('data/raw.csv')
     context['ti'].xcom_push(key='raw_csv_path', value=raw_csv_path)
--- dags/utils/transform_to_ccclub_db_schema.py
+++ dags/utils/transform_to_ccclub_db_schema.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pandas as pd
 
 from transform.contest import transform_contest
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 
 def transform_to_ccclub_db_schema(transform_func, **context):
@@ -13,3 +14,12 @@ def transform_to_ccclub_db_schema(transform_func, **context):
     result_df.to_csv('data/result.csv', index=False)
     result_csv_path = os.path.abspath('data/result.csv')
     context['ti'].xcom_push(key='result_csv_path', value=result_csv_path)
+
+    # upload result csv to s3
+    s3_hook = S3Hook(aws_conn_id="s3_airflow_data")
+    s3_hook.load_file(
+        filename='data/result.csv',
+        key='result.csv',
+        bucket_name="ccclub-airflow-data",
+        replace=True
+    )
