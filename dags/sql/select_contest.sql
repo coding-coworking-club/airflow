2c3efe2ef5fc969a5edd3f85a8113701355fa3f8
waukin <jimmy83882@gmail.com>
2023-03-07 21:06:30 +0800

    refine select_contest.sql

--- dags/sql/select_contest.sql
+++ dags/sql/select_contest.sql
@@ -1,3 +1,4 @@
 SELECT id, title, description, start_time, end_time, create_time, last_update_time
 FROM contest
-WHERE title LIKE 'ccClub 2022 Fall%' OR title like '2022秋%';
+WHERE title LIKE '%2022 Fall%'
+ORDER BY id ASC;
