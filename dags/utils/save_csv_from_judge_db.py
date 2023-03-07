966cc91006349ce9b38812a0ba748ad68febd341
waukin <jimmy83882@gmail.com>
2023-03-07 20:43:11 +0800

    chore: custom csv filename

--- dags/utils/save_csv_from_judge_db.py
+++ dags/utils/save_csv_from_judge_db.py
@@ -8,6 +8,7 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 def save_csv_from_judge_db(sql, **context):
     # connect to judge db
+    filename = sql.split('/')[-1].split('.')[0].split('_')[-1]
     pg_hook = connect_to_judge_db()
     pg_cursor = pg_hook.get_conn().cursor()
 
@@ -19,13 +20,13 @@ def save_csv_from_judge_db(sql, **context):
     # save raw csv
     col_names = [i[0] for i in pg_cursor.description]
     raw_df = pd.DataFrame.from_records(results, columns=col_names)
-    raw_df.to_csv('data/raw.csv', index=False)
+    raw_df.to_csv(f'data/raw_{filename}.csv', index=False)
     s3_hook = S3Hook(aws_conn_id="s3_airflow_data")
     s3_hook.load_file(
-        filename='data/raw.csv',
-        key='data/raw.csv',
+        filename=f'data/raw_{filename}.csv',
+        key=f'data/raw_{filename}.csv',
         bucket_name="ccclub-airflow-data",
         replace=True
     )
-    raw_csv_path = os.path.abspath('data/raw.csv')
+    raw_csv_path = os.path.abspath(f'data/raw_{filename}.csv')
     context['ti'].xcom_push(key='raw_csv_path', value=raw_csv_path)
